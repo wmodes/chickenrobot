b75c6f99bbfadc76579d23dcb1ca231495e0d1d3
Wes Modes <wmodes@gmail.com>
2020-10-23 09:46:18 -0700

    Create steppertest.py

---
+++ experiments/steppertest.py
@@ -0,0 +1,26 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+DIR = 20    # Direction GPIO pin
+STEP = 21   # Step GPIO pin
+CW = 1      # Clockwise rotation
+CCW = 0     # Anti-clockwise rotation
+SPR = 200    # Steps per revolution (360/1.8) from stepper datasheet
+
+step_count = SPR
+delay = 0.005   # 1 second / SPR
+
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+sleep(0.5)
+GPIO.output(DIR, CCW)
+
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay) 

f1bf20d7c5c0263ae7ab3bc3ee4bdccca4bf6d71
Wes Modes <wmodes@gmail.com>
2020-10-28 16:38:14 -0700

    camera experiments

---
+++ camera.py
@@ -0,0 +1,6 @@
+# camera.py - camera class for chickenrobot, a controller for a coop door and cam controller
+# author: Wes Modes <wmodes@gmail.com>
+# date: Oct 2020
+# license: MIT
+
+import cv2
--- experiments/camera.py
+++ experiments/camera.py
@@ -1,9 +1,16 @@
 import cv2
 
-cam = cv2.VideoCapture(0)
-s, im = cam.read() # captures image
+# max resolution of the hbv-1615 is 1280x1024
+
+
+cam1 = cv2.VideoCapture(0)
+cam1.set(3, 1280)
+cam1.set(4, 1024)
+s, im = cam1.read() # captures image
 cv2.imwrite("test1.bmp",im) # writes image test.bmp to disk
 
-cam = cv2.VideoCapture(2)
-s, im = cam.read() # captures image
+cam2 = cv2.VideoCapture(2)
+cam2.set(3, 1280)
+cam2.set(4, 1024)
+s, im = cam2.read() # captures image
 cv2.imwrite("test2.bmp",im) # writes image test.bmp to disk

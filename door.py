63b0485bdfcb796a13857ad3ce4de9c0c3f6d51f
Wes Modes <wmodes@gmail.com>
2020-10-28 19:38:55 -0700

    Store door state

--- door.py
+++ door.py
@@ -12,14 +12,14 @@ if sys.platform == "darwin":
     # sys.modules['smbus'] = fake_rpi.smbus # Fake smbus (I2C)
 import RPi.GPIO as GPIO
 from time import sleep
+import os.path
 
-DIR = 20    # Direction GPIO pin
-STEP = 21   # Step GPIO pin
-CW = 1      # Clockwise rotation
-CCW = 0     # Anti-clockwise rotation
-SPR = 200    # Steps per revolution (360/1.8) from stepper datasheet
-CLOSED = 0
-OPEN = 1
+DIR = 20            # Direction GPIO pin
+STEP = 21           # Step GPIO pin
+CCW = CLOSED = 0    # Anti-clockwise rotation
+CW = OPEN = 1       # Clockwise rotation
+SPR = 200           # Steps per revolution (360/1.8) from stepper datasheet
+DOOR_STATE_FILE = "door.state"
 
 step_count = SPR
 delay = 0.005   # 1 second / SPR
@@ -29,11 +29,27 @@ class Door(object):
 
     def __init__(self, revs):
         self.revs = revs
-        self.status = CLOSED
+        self._read_door_state()
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(DIR, GPIO.OUT)
         GPIO.setup(STEP, GPIO.OUT)
+
+    def _store_door_state(self):
+        with open(DOOR_STATE_FILE, 'w') as file:
+            file.write("DOOR_STATUS=" + str(self.status))
+
+    def _read_door_state(self):
+        if (os.path.isfile(DOOR_STATE_FILE)):
+            with open(DOOR_STATE_FILE, 'r') as file:
+                line = file.read()
+            try:
+                self.status = int(line.split("=")[1].strip())
+            except ValueError:
+                self.status = CLOSED
+        else:
+            self.status = CLOSED
+            self._store_door_state()
 
     def _move_door(self, direction):
         GPIO.output(DIR, direction)
@@ -42,13 +58,14 @@ class Door(object):
             sleep(delay)
             GPIO.output(STEP, GPIO.LOW)
             sleep(delay)
+        self._store_door_state(direction)
 
     def open_door(self):
-        self._move_door(CW)
+        self._move_door(OPEN)
         self.status = OPEN
 
     def close_door(self):
-        self._move_door(CCW)
+        self._move_door(CLOSED)
         self.status = CLOSED
 
     def is_open(self):
